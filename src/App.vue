3974933488b1a15e594580700fb92ab2475d0975
stang464 <68525427+stang464@users.noreply.github.com>
2020-08-20 21:41:41 +0700

    edit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+    <Button></Button>
+    
+</div>
+
+
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
+import Button from './components/Button.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
+    name: 'App',
+    
+    components: {
+      Button
+    },
+    data: function () {
+        return {
+            player: [{
+                    name: 'Anakin',
+                    hp: 100,
+                    image: ''},
+                {
+                    name: 'Ironman',
+                    hp: 120,
+                    image: ''
+                },
+                {
+                    name: 'spiderman',
+                    hp: 110,
+                    image: ''
+                },
+                {
+                    name: 'stat lord',
+                    hp: 129,
+                    image: ''
+                },
+            ],
+            
+        }
+
+    },
+
+   
+
 }
+
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
 }
 </style>
